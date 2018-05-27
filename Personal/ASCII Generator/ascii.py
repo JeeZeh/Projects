b64cf0202d903dbd03b1a1199f0feb1020453616
JeeZeh <Jesse.ashmore97@gmail.com>
2018-05-27 15:03:32 +0100

    .

--- Personal/ASCII Generator/ascii.py
+++ Personal/ASCII Generator/ascii.py
@@ -4,9 +4,17 @@ import os
 
 filelist = os.listdir("D:/Documents/GitHub/Projects/Personal/ASCII Generator/in/")
 
-def ascii(fname, colour, resolution):
+def ascii(fname, colour, resolution, background):
     lum = True
     rgb = False
+
+    if(background == 1):
+        bg_colour = 255
+        fill_colour = 0
+    else:
+        bg_colour = 0
+        fill_colour = 255
+    
     
     lum_dir = os.path.dirname(__file__) + 'out/lum/'
     rgb_dir = os.path.dirname(__file__) + 'out/rgb/'
@@ -40,21 +48,28 @@ def ascii(fname, colour, resolution):
         img_rgb = img_rgb.resize((w, h))
 
     if(lum):
-        lum_out = Image.new('L', (w*spacing, h*spacing), color = 255)
+        lum_out = Image.new('L', (w*spacing, h*spacing), color = bg_colour)
         lum_text = ImageDraw.Draw(lum_out)
       
     if(rgb):
-        rgb_out = Image.new('RGB', (w*spacing, h*spacing), color = (255, 255, 255))
+        rgb_out = Image.new('RGB', (w*spacing, h*spacing), color = (bg_colour, bg_colour, bg_colour))
         rgb_text = ImageDraw.Draw(rgb_out)
         
     lum_map = []
     rgb_map = []
     
-    for y in range(0, h):
-        lum_row = []
-        for x in range(0, w):
-            lum_row.append(11-round(img_lum.getpixel((x,y))/23.2))     
-        lum_map.append(lum_row)
+    if(background == 1):
+        for y in range(0, h):
+            lum_row = []
+            for x in range(0, w):
+                lum_row.append(11-round(img_lum.getpixel((x,y))/23.2))     
+            lum_map.append(lum_row)
+    else:
+        for y in range(0, h):
+            lum_row = []
+            for x in range(0, w):
+                lum_row.append(round(img_lum.getpixel((x,y))/23.2))     
+            lum_map.append(lum_row)
     
     if(rgb):
         for y in range(0, h):
@@ -66,19 +81,22 @@ def ascii(fname, colour, resolution):
     for y in range(0, h):
         for x in range(0, w):
             if(lum):
-                lum_text.text((x*spacing, y*spacing), ascii_12[lum_map[y][x]], fill=0)
+                lum_text.text((x*spacing, y*spacing), ascii_12[lum_map[y][x]], fill=fill_colour)
             if(rgb):
                 rgb_text.text((x*spacing, y*spacing), ascii_12[lum_map[y][x]], fill=(rgb_map[y][x]))
 
     if(lum):
         lum_out.save(lum_dir + fname)
-        print("Saved: " + file + "(B&W)")
+        print("Saved: " + file + " (B&W)")
     if(rgb):
         rgb_out.save(rgb_dir + fname)
-        print("Saved: " + file + "(RGB)")
+        print("Saved: " + file + " (RGB)")
 
 colour = input("Colour Mode? '1' - RGB, '2' - BW or '3' - both\n").lower()
 resolution = input("Resolution? '1' - low, '2' - medium, '3' - high\n").lower()
+background = input("Background? '1' - black on white, '2' - white on black\n")
+
 
 for file in filelist:
-    ascii(file, int(colour), int(resolution))
+    ascii(file, int(colour), int(resolution), int(background))
+    
